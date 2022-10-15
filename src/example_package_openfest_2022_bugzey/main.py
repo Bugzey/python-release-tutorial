431c7e0f24588eb75c5ae29b9bd1f729ed565819
Radoslav Dimitrov <rdimitrov@indeavr.com>
2022-10-15 11:51:24 +0300

    Added demo code, .gitignore, pyproject.toml and half-done github actions

---
+++ src/example_package_openfest_2022_bugzey/main.py
@@ -0,0 +1,13 @@
+def add(base: int, add: int) -> int:
+    """
+    Add two numbers
+
+    Args:
+        base: base number to add to
+        add: number to add to base
+
+    Returns:
+        int
+    """
+    return base + int
+
